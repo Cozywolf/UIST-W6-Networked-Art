c8d56fdd987297e705af54718c27092bf59c6524
Steven Dong <sunlight75@gmail.com>
2016-02-21 01:51:58 -0800

    Add mobile and speech control.

--- sketch.js
+++ sketch.js
@@ -30,55 +30,83 @@ var leafRed;
 var leafGreen;
 var flower = [];
 var flowerLimit = 30;
-var flowerColorRed, flowerColorBlue, flowerColorGreen;
 var star = [];
 var starLimit = 20;
 var cloud = [];
-var cloudLimit = 15;
+var cloudLimit = 10;
 
 var rain = [];
-var rainLimit = 200;
-var rainFall = 0;
+var rainLimit = 400;
+
+var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+myRec.continuous = true; // do continuous recognition
+myRec.interimResults = true; // allow partial recognition (faster, less accurate)
 
 function setup() {
   socket = io(SOCKET_URL + TEAM_NAME); // Open a socket connection to the server.
-  stroke(153, 102, 51);
+
+  greeting = createElement('h2', 'what is your name?');
+  greeting.position(20, 0);
+
+  input = createInput();
+  input.position(20, 60);
+
+  button = createButton('submit');
+  button.position(150, 60);
+  button.mousePressed(addChat);
+
+  textAlign(CENTER)
+  textSize(50);
+
+  messageSpeech = createElement('h3', 'Say "flower" to add flowers, "cloud" to add clouds, and "star" to add star.');
+  messageShake = createElement('h3', 'Shake your hand to rain.');
+
+  messageSpeech.position(20, 80);
+  messageShake.position(20, 120);
+
+  myRec.onResult = showResult;
+  myRec.start();
+
+
+  socket.emit('sense', {
+    deviceShaken: true
+  });
+  socket.on('deviceShaken', deviceShaken);
   socket.on('flower', addFlower);
   socket.on('star', addStar);
   socket.on('cloud', addCloud);
   socket.on('clear', clearAll);
-
-//rain setup
-  for(var i=0;i<rainLimit;i++){
-    addRain();
-  }
+  socket.on('chat', chatPush);
+  //
+  // for (var i = 0; i < rainLimit; i++) {
+  //   addRain();
+  // }
 }
 
 function draw() {
-
   createCanvas(windowWidth, windowHeight);
-  d = 0.45 * windowWidth;
-  x = d * cos(ang);
-  y = d * sin(ang);
+  x = 0.6 * windowWidth * cos(ang);
+  y = 0.95 * windowHeight * sin(ang);
 
   // sky color change
+  r = 250;
+  g = 245;
+  b = 192 + (1 - Math.abs(x / 560)) * 63;
+  dk = Math.abs(x / 560) * 150;
   if (y > 0) {
     background(r, g, b);
   } else {
     background(dk, dk, dk);
   }
-  r = 250;
-  g = 245;
-  b = 192 + (1 - Math.abs(x / 560)) * 63;
-  dk = Math.abs(x / 560) * 150;
 
   //draw star and cloud
   push(); // No.1
   if (y > 0) {
+    star = [];
     for (var i = 0; i < cloud.length; ++i) {
       fill(cloud[i].c);
       noStroke();
-      var pikerandom = random(85, 100);
+      var pikerandom = 0.6 * random(85, 100);
       fill(cloud[i].c)
       ellipse(cloud[i].x, cloud[i].y, pikerandom * 2.3, pikerandom * 0.8);
       ellipse(cloud[i].x + 2 * pikerandom / 3, cloud[i].y - pikerandom / 3, 0.66 * pikerandom, 0.66 * pikerandom);
@@ -86,6 +114,7 @@ function draw() {
     }
   }
   if (y < 0) {
+    cloud = [];
     for (var i = 0; i < star.length; ++i) {
       fill(star[i].c);
       noStroke();
@@ -144,6 +173,7 @@ function draw() {
   translate(-windowWidth / 2, -windowHeight);
   for (var i = 0; i < flower.length; ++i) {
     strokeWeight(3);
+    stroke(51, 153, 51);
     line(flower[i].x, flower[i].y, flower[i].x, flower[i].y - 50);
     fill(flower[i].c);
     strokeWeight(0);
@@ -184,44 +214,36 @@ function draw() {
 
   // draw the first(center) tree
   push(); // No.6
-  stroke(139,69,19);
+  stroke(139, 69, 19);
   branch(0.2 * windowHeight, 0.02 * windowHeight);
   pop(); // No.6
 
   // draw the second (left) tree
   push(); // No.7
-  stroke(112,128,144);
+  stroke(112, 128, 144);
   translate(-windowWidth / 3, -windowHeight / 6)
   branch(0.1 * windowHeight, 0.01 * windowHeight);
   pop(); // No.7
 
   // draw the third (right) tree
   push(); // No.8
-  stroke(165,42,42);
+  stroke(165, 42, 42);
   translate(windowWidth / 3, -windowHeight / 4)
   branch(0.05 * windowHeight, 0.005 * windowHeight);
   pop(); // No.8
 
   pop(); // No.2
 
-  for (var i = 0; i<rainLimit;i++){
-    if (rain[i].x >= 0 && rain[i].y <= windowHeight){
-    rain[i].x-=10;
-    rain[i].y+=10;
-    stroke(100,100,255);
-    strokeWeight(2);
-    line(rain[i].x, rain[i].y, rain[i].x-10, rain[i].y+15);
-  }
-    else{
-    rain[i].x = random(5,windowWidth);
-    rain[i].y = random(0,windowHeight);
+  for (var i = 0; i < rain.length; ++i) {
+    if (rain[i].x >= 0 && rain[i].y <= windowHeight) {
+      rain[i].x -= 15;
+      rain[i].y += 15;
+      stroke(100, 100, 255);
+      strokeWeight(2);
+      line(rain[i].x, rain[i].y, rain[i].x - 15, rain[i].y + 20);
+    } else {
+      rain.pop();
     }
-  }
-  if (rainFall>windowHeight){
-    rainFall = 0;
-  }
-  else {
-    rainFall+=10;
   }
 
 }
@@ -278,11 +300,14 @@ function branch(len, thick) {
   }
 }
 
+
 function addRain() {
+  for (var i = 0; i< 10; i++){
   rain.push({
-    x: random(5,windowWidth-5),
-    y: random(5,windowHeight)
+    x: random(5, windowWidth - 5),
+    y: random(5, windowHeight)
   });
+  }
 }
 
 function addFlower(x, y, r, g, b) {
@@ -312,37 +337,128 @@ function addCloud(x, y, r, g, b) {
   if (cloudLimit > 0 && cloud.length > cloudLimit) cloud.shift();
 }
 
-function mousePressed() {
-  if (mouseY > 0.7 * windowHeight) {
-    flowerColorRed = floor(random(0, 255));
-    flowerColorGreen = floor(random(0, 255));
-    flowerColorBlue = floor(random(0, 255));
-    addFlower(mouseX, mouseY, flowerColorRed, flowerColorGreen, flowerColorBlue);
-    socket.emit('flower', mouseX, mouseY, flowerColorRed, flowerColorGreen, flowerColorBlue);
-  } else if (mouseY < 0.7 * windowHeight && y > 0) {
-    cloudColorRed = floor(random(0, 255));
-    cloudColorGreen = floor(random(0, 255));
-    cloudColorBlue = floor(random(0, 255));
-    addCloud(mouseX, mouseY, cloudColorRed, cloudColorGreen, cloudColorBlue);
-    socket.emit('cloud', mouseX, mouseY, cloudColorRed, cloudColorGreen, cloudColorBlue);
-  } else if (mouseY < 0.7 * windowHeight && y < 0) {
-    starColorRed = floor(random(0, 255));
-    starColorGreen = floor(random(0, 255));
-    starColorBlue = floor(random(0, 255));
-    addStar(mouseX, mouseY, starColorRed, starColorGreen, starColorBlue);
-    socket.emit('star', mouseX, mouseY, starColorRed, starColorGreen, starColorBlue);
-  }
-}
+// function mousePressed() {
+//   if (mouseY > 0.7 * windowHeight) {
+//     flowerColorRed = floor(random(0, 255));
+//     flowerColorGreen = floor(random(0, 255));
+//     flowerColorBlue = floor(random(0, 255));
+//     addFlower(mouseX, mouseY, flowerColorRed, flowerColorGreen, flowerColorBlue);
+//     socket.emit('flower', mouseX, mouseY, flowerColorRed, flowerColorGreen, flowerColorBlue);
+//   } else if (mouseY < 0.7 * windowHeight && y > 0 && mouseY > 80) {
+//     cloudColorRed = floor(random(0, 255));
+//     cloudColorGreen = floor(random(0, 255));
+//     cloudColorBlue = floor(random(0, 255));
+//     addCloud(mouseX, mouseY, cloudColorRed, cloudColorGreen, cloudColorBlue);
+//     socket.emit('cloud', mouseX, mouseY, cloudColorRed, cloudColorGreen, cloudColorBlue);
+//   } else if (mouseY < 0.7 * windowHeight && y < 0) {
+//     starColorRed = floor(random(0, 255));
+//     starColorGreen = floor(random(0, 255));
+//     starColorBlue = floor(random(0, 255));
+//     addStar(mouseX, mouseY, starColorRed, starColorGreen, starColorBlue);
+//     socket.emit('star', mouseX, mouseY, starColorRed, starColorGreen, starColorBlue);
+//   }
+// }
 
-function keyPressed(){
-  if(keyCode===32){
+function keyPressed() {
+  if (keyCode === 18) {
     clearAll();
     socket.emit('clear');
   }
 }
 
-function clearAll(){
+function clearAll() {
   star = [];
   cloud = [];
   flower = [];
 }
+
+function addChat() {
+  var name = input.value();
+  greeting.html('Welcome ' + name + '!');
+  socket.emit('chat', name);
+  input.value('');
+}
+
+function chatPush(text) {
+  greeting.html(text + ' just came to the garden!');
+  input.value('');
+}
+
+function showResult() {
+  var mostrecentword = myRec.resultString.split(' ').pop();
+  if (mostrecentword.indexOf("flower") !== -1) {
+    var flowerX = floor(random(0, windowWidth));
+    var flowerY = floor(random(0.7 * windowHeight, windowHeight));
+    var flowerColorRed = floor(random(0, 255));
+    var flowerColorGreen = floor(random(0, 255));
+    var flowerColorBlue = floor(random(0, 255));
+    addFlower(flowerX, flowerY, flowerColorRed, flowerColorGreen, flowerColorBlue);
+    socket.emit('flower', mouseX, mouseY, flowerColorRed, flowerColorGreen, flowerColorBlue);
+  } else if (mostrecentword.indexOf("star") !== -1) {
+    var starX = floor(random(0, windowWidth));
+    var starY = floor(random(40, 0.7 * windowHeight));
+    var starColorRed = floor(random(0, 255));
+    var starColorGreen = floor(random(0, 255));
+    var starColorBlue = floor(random(0, 255));
+    addStar(starX, starY, starColorRed, starColorGreen, starColorBlue);
+    socket.emit('star', starX, starY, starColorRed, starColorGreen, starColorBlue);
+  } else if (mostrecentword.indexOf("cloud") !== -1) {
+    var cloudX = floor(random(0, windowWidth));
+    var cloudY = floor(random(40, 0.7 * windowHeight));
+    var cloudColorRed = floor(random(0, 255));
+    var cloudColorGreen = floor(random(0, 255));
+    var cloudColorBlue = floor(random(0, 255));
+    addCloud(cloudX, cloudY, cloudColorRed, cloudColorGreen, cloudColorBlue);
+    socket.emit('cloud', mouseX, mouseY, cloudColorRed, cloudColorGreen, cloudColorBlue);
+  }
+  console.log(mostrecentword);
+}
+
+function deviceTurned() {
+  if (orientation == "landscape") {
+    orientation = "portrait";
+  } else if (orientation == "portrait") {
+    orientation = "landscape";
+  }
+  console.log("phone orientation = " + orientation);
+}
+
+function deviceShaken() {
+  addRain();
+  // if (orientation == "landscape"){
+  //draw rain
+  // for (var i = 0; i < rainLimit; i++) {
+  //   if (rain[i].x >= 0 && rain[i].y <= windowHeight) {
+  //     rain[i].x -= 15;
+  //     rain[i].y += 15;
+  //     stroke(100, 100, 255);
+  //     strokeWeight(2);
+  //     line(rain[i].x, rain[i].y, rain[i].x - 10, rain[i].y + 15);
+  //   } else {
+  //     rain[i].x = random(5, windowWidth);
+  //     rain[i].y = random(0, windowHeight);
+  //   }
+  //   console.log("Raining left.")
+  // }
+  // }
+  // else if (orientation == "portrait"){
+  //     //draw rain
+  //     for (var i = 0; i < rainLimit; i++) {
+  //       if (rain[i].x <= windowWidth && rain[i].y <= windowHeight) {
+  //         rain[i].x += 15;
+  //         rain[i].y += 15;
+  //         stroke(100, 100, 255);
+  //         strokeWeight(2);
+  //         line(rain[i].x, rain[i].y, rain[i].x + 10, rain[i].y + 15);
+  //       } else {
+  //         rain[i].x = random(0, windowWidth-5);
+  //         rain[i].y = random(0, windowHeight);
+  //       }
+  //       console.log("Raining right.")
+  //     }
+  // }
+}
+//
+// function raining (){
+//
+// }
